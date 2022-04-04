7d5fb70f0a4c1bdf8bf86eb2b2470219f91c884f
andrewrgarcia <andrewrgarcia@ufl.edu>
2022-04-03 20:38:02 -0400

    update matrix solver msolver.py code to work more efficiently

--- msolver.py
+++ msolver.py
@@ -6,37 +6,27 @@ Created on Thu Jan 30 16:32:05 2020
 """
 '''A x = b matrix solver script
 Andrew Garcia
-Last Update: 02/21/2020'''
+Last Update: 04/03/2022'''
 
 import argparse
 import numpy as np
 
-# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 
-ap.add_argument("-g","--excel_grid", default = False, type = bool,
-                help="'a' matrix declaration: one line terminal input [False] (small matrices) \
-                or extract from Excel file  [True](large matrices) [default: False]")
-ap.add_argument("-gb","--excel_grid_b", default = False, type = bool,
-                help="'b' vector/matrix declaration: one line terminal input [False] \
-                (vectors only) or extract from Excel file  [True](large matrices) \
-                [default: False]")
+ap.add_argument("-pA","--path", default= '', type =str,
+                help="path to excel file containing A matrix e.g. A.xlsx")
+ap.add_argument("-pb","--path_b", default= '', type =str,
+                help="path to excel file containing b matrix e.g. b.xlsx")
 
-ap.add_argument("-p","--path", default= r'Book.xlsx', type =str,
-                help="path to excel file containing A matrix")
-ap.add_argument("-pb","--path_b", default= r'Book2.xlsx', type =str,
-                help="path to excel file containing b matrix")
-
-ap.add_argument("-s","--sheet", default= r'Sheet1', type =str,
+ap.add_argument("-sA","--sheet", default= r'Sheet1', type =str,
                 help="Sheet name of excel file containing A matrix (default: Sheet1)")
 ap.add_argument("-sb","--sheet_b", default= r'Sheet1', type =str,
                 help="Sheet name of excel file containing b vector or matrix (default: Sheet1)")
 
-ap.add_argument("-a","--A_array", nargs ='+', type=float,
+ap.add_argument("-A","--A_array", nargs ='+', type=float,
                 help="A matrix in Ax = b")
-ap.add_argument("-da","--A_dim", nargs ='+',type=int,
+ap.add_argument("-dA","--A_dim", nargs ='+',type=int,
                 help="dimensions of A (type:'-d 6 6' for 6 x 6 matrix)")
-
 
 ap.add_argument("-b", "--b_array", nargs ='+',type =float,
                 help="b vector (or matrix) in Ax = b")
@@ -46,26 +36,16 @@ ap.add_argument("-db","--b_dim", nargs ='+',type=int,default=None,
 args = vars(ap.parse_args())
 
 
-# import xlwings as xw
 import string
 import pandas as pd
 
 
 def excel_matrix(book, sheet):
-    # b00k=xw.Book(book) 
-    # df=pandas.read_excel(book,sheet)
-    # m,n= df.shape
-    # m=m+1
-    # lastcol=list(string.ascii_lowercase)[n-1]
-    # mat = b00k.sheets[sheet].range('a1:'+lastcol+str(m)).value
-    # mat = np.array(mat).reshape(m,n)
-    # b00k.close() 
-    # return mat
     df = pd.read_excel(book, sheet,header=None).to_numpy()
     return df
     
 
-if args["excel_grid_b"] is False:
+if len(args["path_b"]) == 0:
     b = args["b_array"]
     dim_b = args["b_dim"]
     b = np.array(b)
@@ -73,7 +53,9 @@ if args["excel_grid_b"] is False:
 else: 
     b = excel_matrix(args["path_b"], args["sheet_b"])
 
-if args["excel_grid"] is False:
+
+if len(args["path"]) == 0:
+
     a= args["A_array"]
     dim_a = args["A_dim"]
     
